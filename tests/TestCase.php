45f7533384a7c1aef332924efea58b56255d58ea
kkgmkkgm01 <kkgmkkgm01@gmail.com>
2018-12-19 15:53:57 +0900

    TestCase修正

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,14 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function testBoard()
+    {
+        $this->visit('/posts')//  postsページにアクセスしてみる
+        ->see('Recent Comment');//           「Recent Comment」という文字列が見える
+    }
+
+
+
+
 }
