f455d8c8eaf3735fd391bf652ddc35b4d8bc644b
kkgmkkgm01 <kkgmkkgm01@gmail.com>
2018-12-20 16:02:25 +0900

    Laravel Dusk追加

--- config/app.php
+++ config/app.php
@@ -147,6 +147,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Laravel\Socialite\SocialiteServiceProvider::class,
+        Laravel\Dusk\DuskServiceProvider::class,
 
         /*
          * Package Service Providers...
